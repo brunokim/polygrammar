403d3b50c2a83118eff2af7a569750a1a344c2a5
Bruno Kim <brunokim.mc@gmail.com>
2025-09-23 10:37:32 -0300

    Integrate pytest in pre-push

--- polygrammar/test_recursive_parser.py
+++ polygrammar/test_recursive_parser.py
@@ -1,8 +1,12 @@
+import pytest
+
 from polygrammar.model import *
-from polygrammar.recursive_parser import Parser
+from polygrammar.recursive_parser import ParseError, Parser
 
 
 def test_parse_string():
     parser = Parser(Grammar.create(s=String("A")))
     (got,) = parser.first_full_parse("A")
     assert got == ("s", "A")
+    with pytest.raises(ParseError):
+        parser.first_full_parse("B")
